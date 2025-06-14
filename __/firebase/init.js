055b885c7b917344bc90f801ef836db7f0d7d180
Nishanth <nishanth@Nishanths-MacBook-Pro.local>
2025-06-14 09:01:27 +0530

    Modified for 2025 version

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCkh0YYilbY8jRP_Him6QU_UNv-97J4FUM",
+  "authDomain": "nishanth-subramanya.firebaseapp.com",
+  "databaseURL": "https://nishanth-subramanya.firebaseio.com",
+  "messagingSenderId": "479025827084",
+  "projectId": "nishanth-subramanya",
+  "storageBucket": "nishanth-subramanya.appspot.com"
+});
